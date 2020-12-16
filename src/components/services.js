b8071b4b484a71b253084714dd976f3d4f73a19e
Divine Umurerwa <divine.umurerwa@pesachoice.com>
2020-12-16 20:04:39 +0200

    first commit

--- src/components/services.js
+++ src/components/services.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import './services.css';
-import { Link } from 'react-router-dom';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 const Services = (props) => {
